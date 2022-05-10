9bffcb83a6afa90a6e19a3f5b6edde23f6c77235
johnny.thai <johnny.thai@CP15025-JOHNNY>
2022-05-10 14:11:17 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "112.78.2.224",
+    USER: "lua62332_scadatd",
+    PASSWORD: "luanvantotnghiep@2017",
+    DB: "lua62332_scadatotnghiep",
+    dialect: "mssql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
